3f0149176aaa98ffbc3ee313ec230ad500200712
FatherofElias <supra_3_8@hotmail.com>
2024-07-30 10:55:56 -0500

    update q2

---
+++ Question2.py
@@ -0,0 +1,16 @@
+# Question 2
+
+# Task 1
+
+# Import text_utils using an alias
+import text_utils as tu
+
+# Take user input
+user_input = input("Enter a string: ")
+
+# Utilize the functions from text_utils
+reversed_string = tu.reverse_string(user_input)
+capitalized_string = tu.capitalize_string(user_input)
+
+print(f"Reversed String: {reversed_string}")
+print(f"Capitalized String: {capitalized_string}")
